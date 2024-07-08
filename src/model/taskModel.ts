37a7d245ab33bfaa1411a165a8b9c2204bd96d07
dimple <dsaraogi2000@gmail.com>
2024-07-08 22:36:25 +0545

    add json file for tasks

--- src/model/taskModel.ts
+++ src/model/taskModel.ts
@@ -1,7 +1,9 @@
 import { Task } from "../interfaces/taskInterface";
+import { readTasksFromFile, writeTasksToFile } from "../utils/fileUtils";
 
-let tasks: Task[] = [];
-let nextId = 1;
+let tasks: Task[] = readTasksFromFile();
+let nextId =
+  tasks.length > 0 ? Math.max(...tasks.map((task) => task.id)) + 1 : 1;
 
 export const getAllTasks = (): Task[] => tasks;
 
@@ -10,6 +12,7 @@ export const getTaskById = (id: number): Task | undefined =>
 
 export const addTask = (task: Task): Task => {
   tasks.push(task);
+  writeTasksToFile(tasks);
   return task;
 };
 
@@ -17,6 +20,7 @@ export const updateTask = (task: Task): Task | null => {
   const index = tasks.findIndex((t) => t.id === task.id);
   if (index !== -1) {
     tasks[index] = task;
+    writeTasksToFile(tasks);
     return task;
   }
   return null;
@@ -26,6 +30,7 @@ export const removeTask = (id: number): Task | null => {
   const index = tasks.findIndex((task) => task.id === id);
   if (index === -1) return null;
   const [deletedTask] = tasks.splice(index, 1);
+  writeTasksToFile(tasks);
   return deletedTask;
 };
 
