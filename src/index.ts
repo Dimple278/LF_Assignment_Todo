714ef981e2e568604d6f5cd482946f1a2c13e015
dimple <dsaraogi2000@gmail.com>
2024-07-08 14:45:36 +0545

    add CRUD routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import config from "./config";
+import router from "./routes/tasks";
 
 const app = express();
 app.use(express.json());
+
+app.use(router);
 
 app.listen(config.port, () => {
   console.log(`Server listening at port 8080`);
